535fc0e90b612a89cefd594680cd22bbf2eec23b
hmiddeldorp <hugo.middeldorp@alimetry.com>
2024-05-08 11:49:29 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Fix Comment Length
+
+A utility script to lint comments that are longer than 87 characters.
+
+## Usage
+
+`fcl <file_path>`
---
+++ fixcommentlength/main.py
@@ -0,0 +1,38 @@
+"""
+FIX COMMENT LENGTH
+Utility to fix the length of the comments when above 86 chars.
+-----------------------
+Author: Hugo Middeldorp
+"""
+import sys
+import re
+
+
+def process_file(file_path: str):
+    """Given a file path, create a new .temp file with the comment length fixed"""
+    try:
+        with open(file_path, "r", encoding="utf-8") as file:
+            new_lines = []
+            for line in file:
+                # Line (any indentation) that starts with #
+                comment_line = re.match(r"^\s*#\s", line)
+                if comment_line:
+                    comment_start = comment_line.group(0)
+                while len(line) > 87 and comment_line:
+                    # The first (max) 86 characters of the line (won't break words)
+                    split_lines = re.findall(r"(.{0,86})(?:\s)", line)
+                    new_lines.append(split_lines[0] + "\n")
+                    line = f"{comment_start}   {' '.join(split_lines[1:])}\n"
+                new_lines.append(line)
+        with open(file_path, "w", encoding="utf-8") as f:
+            f.writelines(new_lines)
+    except FileNotFoundError:
+        print("File not found:", file_path)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: fcl <file_path>")
+        sys.exit(1)
+    for file_path in sys.argv[1:]:
+        process_file(file_path)
